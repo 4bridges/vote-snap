a07aa9024b8cccbaab9e914040e60e5fade2513a
Web3Degen <hello@4bridges.ch>
2023-11-18 13:39:33 +0100

    :zap: (./docs) clean up

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -11,30 +11,30 @@ import {
 } from "./chunk-WODCYZ5Q.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default3 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default4 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default5 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default6 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default7 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default8 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default9 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default10 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default11 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default12 } from "/home/frank/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default3 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default4 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default5 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default6 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default7 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default8 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default9 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default10 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default11 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default12 } from "C:/Users/simon/Desktop/vote-snap/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/@vueuse/shared/index.mjs
 function tryOnScopeDispose(fn) {
