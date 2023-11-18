57b18ce2d02895eaa205fafed89bf4ea4581fd40
Emad Panah <emad.panah@gmail.com>
2023-11-18 16:27:53 +0330

    change provider to ethers.js, rewrite contracts methods

--- snap/packages/site/src/utils/voteContract.ts
+++ snap/packages/site/src/utils/voteContract.ts
@@ -1,11 +1,66 @@
-import Web3 from 'web3';
-import { abi } from '../../../../../contracts/build/contracts/VoteBuilder.json';
-import { voteContractAddress } from '../config';
-console.log('wwe: ',voteContractAddress);
+import { ethers } from 'ethers';
+import VoteBuilderContractABI from '../../../../../contracts/build/contracts/VoteBuilder.json'; // Import the ABI from the compiled contract
+import { voteContractAddress } from '../config/snap'; // Import the contract address
+import { JsonRpcProviderAddress } from '../config/snap';
 
-let web3;
+console.log('Vote contract address: ', voteContractAddress);
 
-if (typeof window !== 'undefined') web3 = new Web3(window.ethereum);
-else web3 = new Web3();
+const loadContract = async () => {
+    try {
+        const provider = new ethers.providers.JsonRpcProvider(JsonRpcProviderAddress);
+        // Or use your preferred Ethereum node URL
 
-export const voteContract = new web3.eth.Contract(abi, voteContractAddress);
+        const signer = provider.getSigner(); // Replace with your signer if necessary
+        const contract = new ethers.Contract(voteContractAddress, VoteBuilderContractABI.abi, signer);
+
+        const questionTitle = "What is your favorite color?";
+        
+        // Example: Call createVote function
+        // const txCreateVote = await contract.createVote(questionTitle);
+        // await txCreateVote.wait(); // Wait for the transaction to be mined
+
+
+        const createVote = async (questionTitle : string) => {
+            try {
+                const accounts = await provider.listAccounts();
+                const txCreateVote = await contract.createVote(questionTitle).send({ from: accounts[0] });
+                await txCreateVote.wait();
+                console.log('Vote created successfully');
+            } catch (error) {
+                console.error('Error creating vote:', error);
+            }
+        };
+
+        const submitVote = async (questionId : string, option : string) => {
+            try {
+                const accounts = await provider.listAccounts();
+                const txSubmitVote = await contract.submitVote(questionId, option);
+                await txSubmitVote.wait(); 
+                console.log('Vote submitted successfully');
+            } catch (error) {
+                console.error('Error submitted vote:', error);
+            }
+        };
+
+        const getResult = async (questionId : string, option : string) => {
+            try {
+                const accounts = await provider.listAccounts();
+                const txResultVote = await contract.getResult(questionId, option);
+                await txResultVote.wait(); 
+                console.log(`Result for ${option} in question ${questionId}:`, txResultVote);
+        
+                console.log('Vote submitted successfully');
+            } catch (error) {
+                console.error('Error submitted vote:', error);
+            }
+        };
+        
+
+        
+        
+    } catch (error) {
+        console.error('Error interacting with contract:', error);
+    }
+};
+
+loadContract();
