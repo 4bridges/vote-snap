73246366edcb1d462fd24218b8937c6f96f7ee54
FrankBevr <frank_dierolf@web.de>
2023-11-18 14:32:38 +0100

    :zap: (./crossChain) add CCIPSender_Unsafe.sol

---
+++ crossChain/src/CCIPSender_Unsafe.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+
+contract CCIPSender_Unsafe {}
