df6b7013cae7caa71b1486c51687f35b63b2d4a5
FrankBevr <frank_dierolf@web.de>
2023-11-18 21:09:54 +0100

    :zap: (./crossChain) update latest Vote contract

--- crossChain/src/Vote.sol
+++ crossChain/src/Vote.sol
@@ -1,41 +1,40 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.0;
+
+contract VoteFactory {
+    address[] public deployedVotes;
+
+    function createVote(string memory voteTitle) public {
+        address newVote = address(new Vote(voteTitle, msg.sender));
+
+        deployedVotes.push(newVote);
+    }
+
+    function getDeployedVotes() public view returns (address[] memory) {
+        return deployedVotes;
+    }
+}
 
 contract Vote {
-    struct Question {
-        string text;
-        mapping(bytes32 => uint256) votes;
+    address public owner;
+    string public title;
+    mapping(address => bool) public votes;
+    uint public acceptedVoteCount;
+    uint public rejectedVoteCount;
+
+    modifier hasNotVoted() {
+        require(!votes[msg.sender], "You have already voted");
+        _;
     }
 
-    mapping(uint256 => Question) public questions;
-    uint256 public questionCount;
-
-    constructor() {
-        questionCount = 0;
+    constructor(string memory voteTitle, address sender) {
+        owner = sender;
+        title = voteTitle;
     }
 
-    function addQuestion(string memory questionTitle) public {
-        questionCount++;
-        questions[questionCount].text = questionTitle;
-    }
-
-    function vote(uint256 questionId, string memory option) public {
-        bytes32 normalizedOption = keccak256(abi.encodePacked(option));
-        require(
-            normalizedOption == keccak256(abi.encodePacked("Yes")) ||
-                normalizedOption == keccak256(abi.encodePacked("No")),
-            "Invalid option. Only 'Yes' or 'No' are allowed."
-        );
-
-        Question storage q = questions[questionId];
-        q.votes[normalizedOption]++;
-    }
-
-    function getVotes(
-        uint256 questionId,
-        string memory option
-    ) public view returns (uint256) {
-        bytes32 normalizedOption = keccak256(abi.encodePacked(option));
-        return questions[questionId].votes[normalizedOption];
+    function vote(bool _vote) external hasNotVoted {
+        votes[msg.sender] = _vote;
+        if (_vote) acceptedVoteCount++;
+        else rejectedVoteCount++;
     }
 }
