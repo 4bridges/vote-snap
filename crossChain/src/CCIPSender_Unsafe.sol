73ec4273c4d255bc034d07c6865a077df296ddca
FrankBevr <frank_dierolf@web.de>
2023-11-18 14:34:40 +0100

    :zap: (./crossChain) add constructor

--- crossChain/src/CCIPSender_Unsafe.sol
+++ crossChain/src/CCIPSender_Unsafe.sol
@@ -5,4 +5,13 @@ import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.s
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 
-contract CCIPSender_Unsafe {}
+contract CCIPSender_Unsafe {
+    address link;
+    address router;
+
+    constructor(address _link, address _router) {
+        link = _link;
+        router = _router;
+        LinkTokenInterface(link).approve(router, type(uint256).max);
+    }
+}
